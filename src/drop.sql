59ecb32154b59cdff6853d33c34cf99f84b62691
Enzo Hideo <enzohideo@gmail.com>
2023-07-03 14:40:11 -0300

    fix(drop): drop tables with foreign keys first
    It can't drop a table if another table has a foreign key pointing to
    it.
    The solution is to drop tables with foreign keys first. Additionally it
    now verifies if the table exists before trying to drop it.

--- src/drop.sql
+++ src/drop.sql
@@ -1,9 +1,9 @@
-DROP TABLE pessoa,
-  perfil,
-  servico,
-  docente,
-  funcionario,
-  aluno,
-  disciplina,
-  rel_pessoa_perfil,
-  rel_docente_aluno_disciplina;
+DROP TABLE IF EXISTS rel_pessoa_perfil;
+DROP TABLE IF EXISTS rel_docente_aluno_disciplina;
+DROP TABLE IF EXISTS servico;
+DROP TABLE IF EXISTS docente;
+DROP TABLE IF EXISTS funcionario;
+DROP TABLE IF EXISTS aluno;
+DROP TABLE IF EXISTS disciplina;
+DROP TABLE IF EXISTS perfil;
+DROP TABLE IF EXISTS pessoa;
