00fc952a2d455bfa07da7d3bf9665a1e8a8a10db
Jin Shian <cjinshian77@usp.br>
2023-07-03 15:31:00 -0300

    added id's to the tables

--- src/create.sql
+++ src/create.sql
@@ -1,93 +1,107 @@
 
 CREATE TABLE "pessoa"(
+	id SERIAL,
 	cpf CHAR(14) NOT NULL,  /*example: 123.456.789-00*/
 	nome VARCHAR(100) NOT NULL,
 	endereco VARCHAR(255),
-	senha VARCHAR(20) NOT NULL,
+	senha VARCHAR(25) NOT NULL,
 	login VARCHAR(30) NOT NULL,
 	instituicao VARCHAR(50) NOT NULL,
 	data_nascimento DATE,
-	PRIMARY KEY (cpf)    
+	PRIMARY KEY (id),
+	UNIQUE (cpf)    
 );
 
 CREATE TABLE "perfil"(
-	codigo_perfil SERIAL,
-	tipo_perfil VARCHAR(20),
-	PRIMARY KEY (codigo_perfil)
+	id SERIAL,
+	codigo VARCHAR(10) NOT NULL,
+	tipo VARCHAR(20),
+	PRIMARY KEY (codigo_perfil),
+	UNIQUE (codigo)
 );
 
 CREATE TABLE "servico"(
-	codigo_servico SERIAL,	
+	id SERIAL,
+	codigo VARCHAR(10) NOT NULL,	
 	descricao VARCHAR(1000) NOT NULL,
-	tipo_servico VARCHAR(50) NOT NULL,
-	codigo_perfil INT NOT NULL,
+	tipo VARCHAR(50) NOT NULL,
+	id_perfil INT NOT NULL,
 	PRIMARY KEY (codigo_servico),
+	UNIQUE (codigo),
 	CONSTRAINT fk_perfil 
-		FOREIGN KEY (codigo_perfil) 
-			REFERENCES "perfil"(codigo_perfil)  
+		FOREIGN KEY (id) 
+			REFERENCES perfil(id)  
 );
 
 CREATE TABLE "docente"(
+	id SERIAL,
 	cpf_docente CHAR(14) NOT NULL,
 	especialidade VARCHAR(150) NOT NULL,
 	função_tecnica VARCHAR(150),
-	PRIMARY KEY (cpf_docente)
+	PRIMARY KEY (id),
+	UNIQUE (cpf_docente)
 );
 
 CREATE TABLE "funcionario"(
+	id SERIAL,
 	cpf_funcionario CHAR(14) NOT NULL,
 	especialidade VARCHAR(150) NOT NULL,
 	função_tecnica VARCHAR(150),
-	PRIMARY KEY (cpf_docente)
+	PRIMARY KEY (id),
+	UNIQUE (cpf_funcionario)
 );
 
 CREATE TABLE "aluno"(
+	id SERIAL,
 	cpf_aluno CHAR(14) NOT NULL,
 	curso VARCHAR(255) NOT NULL,
 	nota_ingresso DECIMAL(4, 2) NOT NULL,
-	PRIMARY KEY (cpf_aluno)
+	PRIMARY KEY (id),
+	UNIQUE (cpf_aluno)
 );
 
 CREATE TABLE "disciplina"(
-	codigo_disciplina VARCHAR(10) NOT NULL,
+	id SERIAL,
+	codigo VARCHAR(10) NOT NULL,
 	nome VARCHAR(100) NOT NULL,
-	ementa VARCHAR(10000),
+	ementa VARCHAR(1000),
 	data_criacao DATE NOT NULL,
-	PRIMARY KEY (codigo_disciplina)
+	PRIMARY KEY (id),
+	UNIQUE (codigo)
 );
 
 CREATE TABLE "rel_pessoa_perfil"(
-	cpf_pessoa VARCHAR(14) NOT NULL,
-	codigo_perfil INT NOT NULL,
+	id_pessoa INT NOT NULL,
+	id_perfil INT NOT NULL,
 	PRIMARY KEY (cpf_pessoa),
 	PRIMARY KEY (codigo_perfil),
 	CONSTRAINT fk_pessoa
-		FOREIGN KEY cpf_pessoa
-			REFERENCES pessoa(cpf),
+		FOREIGN KEY id_pessoa
+			REFERENCES pessoa(id),
 	CONSTRAINT fk_perfil
-		FOREIGN KEY codigo_perfil
-			REFERENCES perfil(codigo_perfil) 
+		FOREIGN KEY id_perfil
+			REFERENCES perfil(id) 
 );
 
 CREATE TABLE "rel_docente_aluno_disciplina"(
-	cpf_docente VARCHAR(14) NOT NULL,
-	cpf_aluno VARCHAR(14) NOT NULL,
-	codigo_disciplina VARCHAR(10) NOT NULL,
-	nota obtida DECIMAL(4, 2) NOT NULL,
+	id_docente INT NOT NULL,
+	id_aluno INT NOT NULL,
+	id_disciplina INT NOT NULL,
+	nota_obtida DECIMAL(4, 2) NOT NULL,
 	data_inicio DATE NOT NULL,
 	data_fim DATE NOT NULL,
-	PRIMARY KEY cpf_docente,
-	PRIMARY KEY cpf_aluno,
-	PRIMARY KEY codigo_disciplina,
+	PRIMARY KEY id_docente,
+	PRIMARY KEY id_aluno,
+	PRIMARY KEY id_disciplina,
 	CONSTRAINT fk_docente
-		FOREIGN KEY cpf_docente
-			REFERENCES docente(cpf_docente),
+		FOREIGN KEY id_docente
+			REFERENCES docente(id),
 	CONSTRAINT fk_aluno
-		FOREIGN KEY cpf_aluno
-			REFERENCES aluno(cpf_aluno),
+		FOREIGN KEY id_aluno
+			REFERENCES aluno(id),
 	CONSTRAINT fk_disciplina
-		FOREIGN KEY disciplina
-			REFERENCES disciplina(codigo_disciplina)
+		FOREIGN KEY id_disciplina
+			REFERENCES disciplina(id)
 );
 
 	
