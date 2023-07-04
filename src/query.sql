098806455f2162389fbb4f7e4e1d7684264398f4
Jin Shian <cjinshian77@usp.br>
2023-07-04 08:32:54 -0300

    added last three sql functions prototypes

--- src/query.sql
+++ src/query.sql
@@ -10,4 +10,53 @@ BEGIN
   FROM rel_pessoa_perfil AS rpp, servico
   WHERE rpp.id_perfil = servico.id_perfil;
 END
+$$;	
+
+-- Consulta 2
+-- Liste em ordem crescente o total de serviços utilizados agrupados pelo perfil de pessoas
+
+CREATE OR REPLACE FUNCTION total_servicos_por_perfil()
+RETURNS TABLE()
+LANGUAGE plpgsql AS
+$$
+BEGIN
+	RETURN QUERY
+	SELECT 
+	FROM 
+	WHERE
+	GROUP BY 
+	ORDER BY
+END
+$$;
+
+-- Consulta 3
+-- Liste as 5 (cinco) disciplinas mais oferecidas com seus respectivos professores e alunos
+
+CREATE OR REPLACE FUNCTION 5_disciplinas_mais_oferecidas()
+RETURNS TABLE()
+LANGUAGE plpgsql AS
+$$
+BEGIN
+	RETURN QUERY
+	SELECT
+	FROM 
+	WHERE
+	ORDER BY
+END
+$$;
+
+-- Consulta 4
+-- Liste os 5 docentes que mais ministraram disciplinas oferecidas no período de maio de 2020 a maio de 2023
+
+CREATE OR REPLACE FUNCTION 5_docentes_que_mais_ministraram()
+RETURNS TABLE()
+LANGUAGE plpgsql AS
+$$
+BEGIN
+	RETURN QUERY
+	SELECT
+	FROM 
+	WHERE
+	ORDER BY
+END
 $$;
