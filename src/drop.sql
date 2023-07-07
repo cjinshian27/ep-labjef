51a0c677ec693de28c7a063284812a1634dc54fc
Enzo Hideo <enzohideo@gmail.com>
2023-07-07 00:52:34 -0300

    feat(drop.sql): drop functions
    Previously the user had to manually drop each function, but now the
    drop.sql script also drops the functions created by query.sql

--- src/drop.sql
+++ src/drop.sql
@@ -8,3 +8,8 @@ DROP TABLE IF EXISTS aluno;
 DROP TABLE IF EXISTS disciplina;
 DROP TABLE IF EXISTS perfil;
 DROP TABLE IF EXISTS pessoa;
+
+DROP FUNCTION IF EXISTS servicos_por_pessoa ;
+DROP FUNCTION IF EXISTS servicos_utilizados_por_perfil ;
+DROP FUNCTION IF EXISTS cinco_disciplinas_mais_oferecidas ;
+DROP FUNCTION IF EXISTS cinco_docentes_que_mais_ministraram ;
